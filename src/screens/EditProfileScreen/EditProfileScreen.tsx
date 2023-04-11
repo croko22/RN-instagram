e2d7aa09b472ac53abf7901eda244c242c410b31
kchambit <kev132011@hotmail.com>
2023-04-11 17:16:29 -0500

    L-16: React hook form library, rules, values and more

--- src/screens/EditProfileScreen/EditProfileScreen.tsx
+++ src/screens/EditProfileScreen/EditProfileScreen.tsx
@@ -1,40 +1,127 @@
 import { View, Text, StyleSheet, Image, TextInput } from "react-native";
+import { useForm, Controller, Control } from "react-hook-form";
 import user from "../../assets/data/user.json";
 import colors from "../../theme/colors";
 import fonts from "../../theme/fonts";
+import { IUser } from "../../types/models";
+
+const URL_REGEX = /^(ftp|http|https):\/\/[^ "]+$/;
+
+type IEditableUserFields = "name" | "username" | "website" | "bio";
+type IEditableUser = Pick<IUser, IEditableUserFields>;
 
 interface ICustomInput {
+  control: Control<IEditableUser, object>;
   label: string;
-  placeholder: string;
+  name: IEditableUserFields;
   multiline?: boolean;
+  rules?: object;
 }
 
-const CustomInput = ({ label, placeholder, multiline }: ICustomInput) => (
-  <View style={styles.inputContainer}>
-    <Text style={styles.label}>{label}</Text>
-    <TextInput
-      placeholder={placeholder}
-      style={styles.input}
-      multiline={multiline}
-    />
-  </View>
+const CustomInput = ({
+  control,
+  label,
+  name,
+  multiline = false,
+  rules = {},
+}: ICustomInput) => (
+  <Controller
+    control={control}
+    name={name}
+    rules={rules}
+    render={({ field: { onChange, value, onBlur }, fieldState: { error } }) => {
+      return (
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>{label}</Text>
+          <View style={{ flex: 1 }}>
+            <TextInput
+              value={value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              style={[
+                styles.input,
+                { borderColor: error ? colors.error : colors.border },
+              ]}
+              multiline={multiline}
+            />
+            {error && (
+              <Text style={{ color: colors.error }}>
+                {error.message || "Error"}
+              </Text>
+            )}
+          </View>
+        </View>
+      );
+    }}
+  />
 );
 
 const EditProfileScreen = () => {
-  const onSubmit = () => {
-    console.warn("Submit");
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IEditableUser>({
+    defaultValues: {
+      name: user.name,
+      username: user.username,
+      website: user.website,
+      bio: user.bio,
+    },
+  });
+
+  const onSubmit = (data: IEditableUser) => {
+    console.warn("Submit", data);
   };
   return (
     <View style={styles.page}>
       <Image source={{ uri: user.image }} style={styles.avatar} />
       <Text style={styles.textButton}>EditProfileScreen</Text>
 
-      <CustomInput label="Name" placeholder={user.name} />
-      <CustomInput label="Username" placeholder={user.username} />
-      <CustomInput label="Website" placeholder="Website" />
-      <CustomInput label="Bio" placeholder="Bio" multiline />
+      <CustomInput
+        control={control}
+        name="name"
+        label="Name"
+        rules={{ required: "Name is required" }}
+      />
+      <CustomInput
+        control={control}
+        name="username"
+        label="Username"
+        rules={{
+          required: "Username is required",
+          minLength: {
+            value: 3,
+            message: "Username should be more than 3 characters",
+          },
+        }}
+      />
+      <CustomInput
+        control={control}
+        name="website"
+        label="Website"
+        rules={{
+          required: "Website is required",
+          pattern: {
+            value: URL_REGEX,
+            message: "Website should be a valid URL",
+          },
+        }}
+      />
+      <CustomInput
+        control={control}
+        name="bio"
+        label="Bio"
+        multiline
+        rules={{
+          maxLength: {
+            value: 200,
+            message: "Bio should be less than 200 characters",
+          },
+        }}
+      />
 
-      <Text onPress={onSubmit} style={styles.textButton}>
+      <Text onPress={handleSubmit(onSubmit)} style={styles.textButton}>
         Submit
       </Text>
     </View>
@@ -60,10 +147,8 @@ const styles = StyleSheet.create({
     width: 75,
   },
   input: {
-    flex: 1,
     borderColor: colors.border,
     borderBottomWidth: 1,
-    padding: 5,
   },
 });
 
