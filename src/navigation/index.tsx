324e59c32ecfd125f1250f88ad6873df583871db
kchambit <kev132011@hotmail.com>
2023-04-11 17:16:29 -0500

    L-20: Stack navigation, header customization

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,11 +1,39 @@
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "../screens/HomeScreen/HomeScreen";
+import ProfileScreen from "../screens/ProfileScreen/ProfileScreen";
+import { Image } from "react-native";
+import logo from "../assets/images/logo.png";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <HomeScreen />
+      <Stack.Navigator initialRouteName="Feed">
+        <Stack.Screen
+          name="Feed"
+          component={HomeScreen}
+          options={{ headerTitle: HeaderTitle, headerTitleAlign: "center" }}
+        />
+        <Stack.Screen
+          name="UserProfile"
+          component={ProfileScreen}
+          options={{ title: "Profile" }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
+  );
+};
+
+const HeaderTitle = () => {
+  return (
+    <Image
+      source={logo}
+      resizeMode="contain"
+      style={{ width: 150, height: 40 }}
+    />
   );
 };
 
