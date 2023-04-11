e175b30bd7470a4541417d1d8df34b8718582240
kchambit <kev132011@hotmail.com>
2023-04-11 17:16:29 -0500

    L-19: Navigation setup

--- App.tsx
+++ App.tsx
@@ -1,25 +1,12 @@
-import { View, StyleSheet } from "react-native";
-import HomeScreen from "./src/screens/HomeScreen/HomeScreen";
-import CommentsScreen from "./src/screens/CommentsScreen/CommentsScreen";
-import ProfileScreen from "./src/screens/ProfileScreen/ProfileScreen";
-import EditProfileScreen from "./src/screens/EditProfileScreen";
-import PostUploadScreen from "./src/screens/PostUploadScreen";
+// import HomeScreen from "./src/screens/HomeScreen/HomeScreen";
+// import CommentsScreen from "./src/screens/CommentsScreen/CommentsScreen";
+// import ProfileScreen from "./src/screens/ProfileScreen/ProfileScreen";
+// import EditProfileScreen from "./src/screens/EditProfileScreen";
+// import PostUploadScreen from "./src/screens/PostUploadScreen";
+import Navigation from "./src/navigation";
 
 const App = () => {
-  return (
-    <View style={styles.app}>
-      {/* <HomeScreen /> */}
-      {/* <CommentsScreen /> */}
-      {/* <ProfileScreen /> */}
-      {/* <EditProfileScreen /> */}
-      <PostUploadScreen />
-    </View>
-  );
+  return <Navigation />;
 };
-const styles = StyleSheet.create({
-  app: {
-    paddingTop: 30,
-    flex: 1,
-  },
-});
+
 export default App;
