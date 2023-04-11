c09a7200feaa2c36e72ce4b884e3c6aaafd7d68c
kchambit <kev132011@hotmail.com>
2023-04-11 17:16:29 -0500

    Added safeAreaProvider

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-// import HomeScreen from "./src/screens/HomeScreen/HomeScreen";
-// import CommentsScreen from "./src/screens/CommentsScreen/CommentsScreen";
-// import ProfileScreen from "./src/screens/ProfileScreen/ProfileScreen";
-// import EditProfileScreen from "./src/screens/EditProfileScreen";
-// import PostUploadScreen from "./src/screens/PostUploadScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import Navigation from "./src/navigation";
 
 const App = () => {
-  return <Navigation />;
+  return (
+    <SafeAreaProvider>
+      <Navigation />
+    </SafeAreaProvider>
+  );
 };
 
 export default App;
