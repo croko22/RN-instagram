2d102104103f8a2e6e013296023ffc848244283e
kchambit <kev132011@hotmail.com>
2023-04-11 17:16:28 -0500

    Data and types for (image, images, video) post

--- src/types/models.ts
+++ src/types/models.ts
@@ -4,6 +4,7 @@ export interface IPost {
     createdAt: string;
     image?: string;
     images?: string[];
+    video?: string;
     description: string;
     user: IUser;
     nofComments: number;
