8559c7522f5a2bd866ba5ffd81f2258ff38db61c
kchambit <kev132011@hotmail.com>
2023-04-11 17:16:29 -0500

    L-26: Tob Tab Bar - UserSearch - PostsSearch

---
+++ src/navigation/SearchTabNavigator.tsx
@@ -0,0 +1,26 @@
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import CommentsScreen from "../screens/CommentsScreen/CommentsScreen";
+import colors from "../theme/colors";
+import { SearchTabNavigationParamList } from "./types";
+import UserSearchScreen from "../screens/UserSearchScreen/UserSearchScreen";
+
+const Tab = createMaterialTopTabNavigator<SearchTabNavigationParamList>();
+
+const SearchTabNavigator = () => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: { paddingTop: insets.top },
+        tabBarIndicatorStyle: { backgroundColor: colors.primary },
+      }}
+    >
+      <Tab.Screen name="Users" component={UserSearchScreen} />
+      <Tab.Screen name="Posts" component={CommentsScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default SearchTabNavigator;
