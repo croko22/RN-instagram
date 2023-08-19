d758a82d7b75e3887bccc4409619297c57a3b775
croko22 <kev132011@hotmail.com>
2023-08-19 12:34:46 -0500

    c: List users on SearchScreen

--- src/screens/UserSearchScreen/UserSearchScreen.tsx
+++ src/screens/UserSearchScreen/UserSearchScreen.tsx
@@ -1,12 +1,33 @@
-import { FlatList } from "react-native";
-import users from "../../assets/data/users.json";
+import { ActivityIndicator, FlatList } from "react-native";
 import UserListItem from "../../components/UserListItem/UserListItem";
+import { useQuery } from "@apollo/client";
+import { listUsers } from "./queries";
+import ApiErrorMessage from "../../components/ApiErrorMessage/ApiErrorMessage";
+import { ListLikesQueryVariables, ListUsersQuery } from "../../API";
 
 const UserSearchScreen = () => {
+  const { data, loading, error, refetch } = useQuery<
+    ListUsersQuery,
+    ListLikesQueryVariables
+  >(listUsers);
+  const users = (data?.listUsers?.items || []).filter((u) => u && !u?._deleted);
+
+  if (loading) return <ActivityIndicator />;
+  if (error || !users)
+    return (
+      <ApiErrorMessage
+        title="Error fetching the user"
+        message={error?.message || "User not found"}
+        onRetry={() => refetch()}
+      />
+    );
+
   return (
     <FlatList
       data={users}
-      renderItem={({ item }) => <UserListItem user={item} />}
+      renderItem={({ item }) => item && <UserListItem user={item} />}
+      onRefresh={() => refetch()}
+      refreshing={loading}
     />
   );
 };
