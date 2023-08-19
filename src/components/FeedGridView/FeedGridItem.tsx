3c7020a19d52a44dbe344e0f2f309e8829972b12
croko22 <kev132011@hotmail.com>
2023-08-19 12:31:30 -0500

    c: Update types for Post

--- src/components/FeedGridView/FeedGridItem.tsx
+++ src/components/FeedGridView/FeedGridItem.tsx
@@ -1,13 +1,13 @@
 import { Image, View } from "react-native";
-import { IPost } from "../../types/models";
 import { MaterialIcons } from "@expo/vector-icons";
 import colors from "../../theme/colors";
+import { Post } from "../../API";
 
-const FeedGridItem = ({ post }: { post: IPost }) => {
+const FeedGridItem = ({ post }: { post: Post }) => {
   return (
     <View style={{ flex: 1, margin: 1, aspectRatio: 1, maxWidth: "33%" }}>
       <Image
-        source={{ uri: post.image || post.images[0] }}
+        source={{ uri: post.image || post.images?.[0] }}
         style={{ flex: 1 }}
       />
       {post.images && (
