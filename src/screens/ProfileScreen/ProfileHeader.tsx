3f6a3b6d5eaca9457ce9ddca442ac0d61ffba9c9
croko22 <kev132011@hotmail.com>
2023-08-19 12:37:00 -0500

    f: Display user data from gql

--- src/screens/ProfileScreen/ProfileHeader.tsx
+++ src/screens/ProfileScreen/ProfileHeader.tsx
@@ -1,30 +1,40 @@
 import { View, Text, Image } from "react-native";
 import { Auth } from "aws-amplify";
-import styles from "./styles";
-import user from "../../assets/data/user.json";
 import Button from "../../components/Button";
+import styles from "./styles";
 import { useNavigation } from "@react-navigation/native";
 import { ProfileNavigationProp } from "../../types/navigation";
+import { User } from "../../API";
+import { DEFAULT_USER_IMAGE } from "../../config";
+import { useAuthContext } from "../../contexts/AuthContext";
 
-const ProfileHeader = () => {
+interface IProfileHeader {
+  user: User;
+}
+
+const ProfileHeader = ({ user }: IProfileHeader) => {
+  const { userId } = useAuthContext();
   const navigation = useNavigation<ProfileNavigationProp>();
   return (
     <View style={styles.root}>
       <View style={styles.headerRow}>
         {/* Profile pic */}
-        <Image source={{ uri: user.image }} style={styles.avatar} />
+        <Image
+          source={{ uri: user.image || DEFAULT_USER_IMAGE }}
+          style={styles.avatar}
+        />
 
         {/* Posts, followers, folling number */}
         <View style={styles.statsContainer}>
-          <Text style={styles.numberText}>77</Text>
+          <Text style={styles.numberText}>{user.nofPosts}</Text>
           <Text>Posts</Text>
         </View>
         <View style={styles.statsContainer}>
-          <Text style={styles.numberText}>7</Text>
+          <Text style={styles.numberText}>{user.nofFollowers}</Text>
           <Text>Followers</Text>
         </View>
         <View style={styles.statsContainer}>
-          <Text style={styles.numberText}>772</Text>
+          <Text style={styles.numberText}>{user.nofFollowings}</Text>
           <Text>Following</Text>
         </View>
       </View>
@@ -33,13 +43,15 @@ const ProfileHeader = () => {
       <Text style={styles.name}>{user.name}</Text>
       <Text>{user.bio}</Text>
 
-      <View style={{ flexDirection: "row" }}>
-        <Button
-          text="Edit Profile"
-          onPress={() => navigation.navigate("Edit Profile")}
-        />
-        <Button text="Sign Out" onPress={() => Auth.signOut()} />
-      </View>
+      {user.id === userId && (
+        <View style={{ flexDirection: "row" }}>
+          <Button
+            text="Edit Profile"
+            onPress={() => navigation.navigate("Edit Profile")}
+          />
+          <Button text="Sign Out" onPress={() => Auth.signOut()} />
+        </View>
+      )}
     </View>
   );
 };
--- src/screens/ProfileScreen/ProfileScreen.tsx
+++ src/screens/ProfileScreen/ProfileScreen.tsx
@@ -1,5 +1,3 @@
-import { Image, FlatList } from "react-native";
-import user from "../../assets/data/user.json";
 import FeedGridView from "../../components/FeedGridView/FeedGridView";
 import ProfileHeader from "./ProfileHeader";
 import { useNavigation, useRoute } from "@react-navigation/native";
@@ -10,22 +8,49 @@ import {
   UserProfileRouteProp,
 } from "../../types/navigation";
 import { useAuthContext } from "../../contexts/AuthContext";
+import { useQuery } from "@apollo/client";
+import { getUser } from "./queries";
+import { ActivityIndicator } from "react-native";
+import ApiErrorMessage from "../../components/ApiErrorMessage/ApiErrorMessage";
+import { GetUserQuery, GetUserQueryVariables } from "../../API";
 
 const ProfileScreen = () => {
   const route = useRoute<UserProfileRouteProp | MyProfileRouteProp>();
   const navigation = useNavigation<
     UserProfileNavigationProp | MyProfileNavigationProp
   >();
-  //* Is better to send an identifier than the whole object
-  // const { userId: authUserId } = useAuthContext().user;
-  // const userID = route.params?.userID || authUserId;
-  // const user = users.find((user) => user.id === userID);
+  const { userId: authUserId } = useAuthContext();
+  const userID = route.params?.userID || authUserId;
   //*Query the user
 
-  navigation.setOptions({ title: user.username });
-  //*Pics gallery
-  //@ts-ignore
-  return <FeedGridView ListHeaderComponent={ProfileHeader} data={user.posts} />;
+  const { data, loading, error, refetch } = useQuery<
+    GetUserQuery,
+    GetUserQueryVariables
+  >(getUser, {
+    variables: {
+      id: userID,
+    },
+  });
+  const user = data?.getUser;
+  navigation.setOptions({ title: user?.username || "Profile" });
+
+  if (loading) return <ActivityIndicator />;
+  if (error || !user)
+    return (
+      <ApiErrorMessage
+        title="Error fetching the user"
+        message={error?.message || "User not found"}
+        onRetry={() => refetch()}
+      />
+    );
+  return (
+    <FeedGridView
+      ListHeaderComponent={() => <ProfileHeader user={user} />}
+      data={user.Posts?.items || []}
+      refetch={refetch}
+      loading={loading}
+    />
+  );
 };
 
 export default ProfileScreen;
