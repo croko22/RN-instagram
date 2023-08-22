d9c03f357bf5e77d8d7549d7fe847aed122f5b79
croko22 <kev132011@hotmail.com>
2023-08-22 17:39:17 -0500

    c: Refresh feed

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -13,7 +13,7 @@ import ApiErrorMessage from "../../components/ApiErrorMessage/ApiErrorMessage";
 
 const HomeScreen = () => {
   const [activePostId, setActivePostId] = useState<string | null>(null);
-  const { data, loading, error } = useQuery<
+  const { data, loading, error, refetch } = useQuery<
     ListPostsQuery,
     ListPostsQueryVariables
   >(listPosts);
@@ -36,7 +36,9 @@ const HomeScreen = () => {
       <ApiErrorMessage title="Error fetching posts" message={error.message} />
     );
 
-  const posts = data?.listPosts?.items || [];
+  const posts = (data?.listPosts?.items || []).filter(
+    (post) => !post?._deleted
+  );
 
   return (
     <FlatList
@@ -47,6 +49,8 @@ const HomeScreen = () => {
       showsVerticalScrollIndicator={false}
       onViewableItemsChanged={onViewableItemsChanged.current}
       viewabilityConfig={viewabilityConfig}
+      onRefresh={() => refetch()}
+      refreshing={loading}
     ></FlatList>
   );
 };
